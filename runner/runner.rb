f62522478412031858035cc1917381dc450aa004
Tyler Komoroske <tjkomor@clemson.edu>
2015-09-07 23:24:48 -0600

    refactored FileIO

--- runner/runner.rb
+++ runner/runner.rb
@@ -8,6 +8,12 @@ input.each_line do |line|
   collection << line.gsub("\n",'').to_i
 end
 
+tree = Tree.new(collection[0])
+collection[1..-1].each do |num|
+  tree.insert(num)
+end
+
+sorted = tree.sort.join("\n")
+
 output = File.open(ARGV[1], 'w')
-sorted = collection.sort.join("\n")
 output.write(sorted)
